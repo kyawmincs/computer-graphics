1c5cfff3d7cb1288db6fa4dd07a5a1edea12985e
Kyaw Min Oo <kyawmin.dac@gmail.com>
2026-01-11 00:48:29 -0800

    yellow 255 to 1

--- glsl/sphere.fs.glsl
+++ glsl/sphere.fs.glsl
@@ -1,4 +1,4 @@
 void main() {
- 	// TODO: Set final rendered color here
-  	gl_FragColor = vec4(255.0, 255.0, 0.0, 1.0);
+ 	// Set final rendered color to yellow
+  	gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
 }
